45e3230dde13ed8bd5995197f4b115af4dcb1ad3
KnightsOfTheSeaA <Knightsofakko@gmail.com>
2017-02-06 21:50:57 +0200

    angle and distance function for navigation

--- src/Subsystems/DriveSubS.cpp
+++ src/Subsystems/DriveSubS.cpp
@@ -8,7 +8,8 @@
 #include <ADXRS450_Gyro.h>
 #include <BuiltInAccelerometer.h>
 
-DriveSubS::DriveSubS() : Subsystem("ExampleSubsystem") {
+DriveSubS::DriveSubS() : Subsystem("ExampleSubsystem")
+{
 	// Driving motor initializations
 	//leftMotor = new Spark(LEFT_MOTOR);
 	//frc::RobotDrive robotDrive { 0, 1 };
@@ -21,10 +22,18 @@ DriveSubS::DriveSubS() : Subsystem("ExampleSubsystem") {
 	Gyro = new ADXRS450_Gyro(SPI::Port::kOnboardCS0);
 	Gyro->Reset();
 	Acceler = new BuiltInAccelerometer(BuiltInAccelerometer::Range::kRange_4G);
+
+	Encoder0 = new Encoder(0, 1, false, 2); // Init right motor encoder
+	Encoder1 = new Encoder(2, 4, false, 2); // Init left motor encoder
+
+	Encoder0->SetDistancePerPulse(WHEELDIAMETER/ENCODERDIVITION);
+
+
 }
 
 
-void DriveSubS::InitDefaultCommand() {
+void DriveSubS::InitDefaultCommand()
+{
 	// Set the default command for a subsystem here.
 	// SetDefaultCommand(new MySpecialCommand());
 	SetDefaultCommand(new Drive());
@@ -33,10 +42,43 @@ void DriveSubS::InitDefaultCommand() {
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
 
-void DriveSubS::RotateAngle(float Angle){
+void DriveSubS::RotateAngle(float Angle, double Speed)
+{
 
-	CommandBase::driveSubS->robotDrive->SetLeftRightMotorOutputs();
-	CommandBase::driveSubS->Gyro->GetAngle());
+	int DAngle= CommandBase::driveSubS->Gyro->GetAngle() + Angle;
 
+	short D = abs(DAngle) / DAngle;
+
+	while(CommandBase::driveSubS->Gyro->GetAngle() > DAngle + 2 || 	CommandBase::driveSubS->Gyro->GetAngle() < DAngle - 2 )
+	{
+
+
+		CommandBase::driveSubS->robotDrive->SetLeftRightMotorOutputs(Speed*D,Speed*D);
+
+	}
+
+	CommandBase::driveSubS->Gyro->GetAngle();
+
+
+}
+
+void DriveSubS::DriveDistance(long UDistance, short Direction, double Speed){
+
+
+	//Distance[0] = CommandBase::driveSubS->Encoder0->GetDistance();
+	//Distance[1] = CommandBase::driveSubS->Encoder1->GetDistance();
+	CommandBase::driveSubS->Encoder0->Reset();
+	CommandBase::driveSubS->Encoder1->Reset();
+
+	while(UDistance <= CommandBase::driveSubS->Encoder1->GetDistance()){
+
+	switch(Direction){
+
+		case 0:CommandBase::driveSubS->robotDrive->SetLeftRightMotorOutputs(Speed, -Speed);break;
+		case 1:CommandBase::driveSubS->robotDrive->SetLeftRightMotorOutputs(-Speed, Speed);break;
+
+	}
+
+	}
 
 }
--- src/Subsystems/DriveSubS.h
+++ src/Subsystems/DriveSubS.h
@@ -6,6 +6,9 @@
 #include <ADXRS450_Gyro.h>
 #include <BuiltInAccelerometer.h>
 
+#define WHEELDIAMETER 100 // Wheel diameter in mm
+#define ENCODERDIVITION 20 // Set encoder divition
+
 class DriveSubS : public Subsystem {
 private:
 	// It's desirable that everything possible under private except
@@ -13,13 +16,19 @@ private:
 
 	//ADXRS450_Gyro Gyro;
 
+	long Distance[2];
+
+
 public:
 	RobotDrive* robotDrive;
 	ADXRS450_Gyro* Gyro;
 	BuiltInAccelerometer* Acceler;
+	Encoder* Encoder0;
+	Encoder* Encoder1;
 	DriveSubS();
 	void InitDefaultCommand();
-	void RotateAngle(float Angle);
+	void RotateAngle(float Angle, double Speed);
+	void DriveDistance(long UDistance, short Direction, double Speed);
 };
 
 #endif  // DriveSubS_H
